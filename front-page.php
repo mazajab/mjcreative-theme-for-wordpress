03fa56a270f4a39daa63a3ec3edca8b59e257acf
mazajab <mazen_musa2000@yahoo.com>
2018-11-11 05:42:54 -0500

    Add files via upload

---
+++ front-page.php
@@ -0,0 +1,201 @@
+<?php get_header(); ?>
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">We've got what you need!</h2>
+            <hr class="light my-4">
+            <p class="text-faded mb-4">Because it’s not just about having a nice looking, modern website. Anyone can do it. The idea is to have a converting one. Your website will only convert when it is properly planned, designed and tested.</p>
+            <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">At Your Service</h2>
+            <hr class="my-4">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-gem text-primary mb-3 sr-icon-1"></i>
+              <h3 class="mb-3">Web Design</h3>
+              <p class="text-muted mb-0">You can tell from our company name that we love building websites around the clock.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-paper-plane text-primary mb-3 sr-icon-2"></i>
+              <h3 class="mb-3">Graphic Design</h3>
+              <p class="text-muted mb-0">Whether you need a logo or fliers for your grand opening, we can design eye-catching graphics that will wow your potential customers.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-code text-primary mb-3 sr-icon-3"></i>
+              <h3 class="mb-3">E Commerce</h3>
+              <p class="text-muted mb-0">Need to sell your best selling book or award winning pies online? We can build you a stellar e-commerce website that safely take credit card payments.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-heart text-primary mb-3 sr-icon-4"></i>
+              <h3 class="mb-3">Digital Marketing</h3>
+              <p class="text-muted mb-0">With Digital Marketing, you can gain more traffic and obtain more fans by creating shareable content such as videos and blog posts.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="p-0" id="portfolio">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php bloginfo('template_directory');?>/img/portfolio/fullsize/1.jpg">
+              <img class="img-fluid" src="<?php bloginfo('template_directory');?>/img/portfolio/thumbnails/1.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project 1
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php bloginfo('template_directory');?>/img/portfolio/fullsize/2.jpg">
+              <img class="img-fluid" src="<?php bloginfo('template_directory');?>/img/portfolio/thumbnails/2.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project 2
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php bloginfo('template_directory');?>/img/portfolio/fullsize/3.jpg">
+              <img class="img-fluid" src="<?php bloginfo('template_directory');?>/img/portfolio/thumbnails/3.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project 3
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php bloginfo('template_directory');?>/img/portfolio/fullsize/4.jpg">
+              <img class="img-fluid" src="<?php bloginfo('template_directory');?>/img/portfolio/thumbnails/4.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project 4
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php bloginfo('template_directory');?>/img/portfolio/fullsize/5.jpg">
+              <img class="img-fluid" src="<?php bloginfo('template_directory');?>/img/portfolio/thumbnails/5.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project 5
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="<?php bloginfo('template_directory');?>/img/portfolio/fullsize/6.jpg">
+              <img class="img-fluid" src="<?php bloginfo('template_directory');?>/img/portfolio/thumbnails/6.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Project 6
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="bg-dark text-white">
+      <div class="container text-center">
+        <h2 class="mb-4"> More Projects!</h2>
+        <a class="btn btn-light btn-xl sr-button" href="#">See More!</a>
+      </div>
+    </section>
+
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Let's Get In Touch!</h2>
+            <hr class="my-4">
+            <p class="mb-5">Ready to start your next project with us? That's great!</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+      
+          </div>
+
+          <div class="col-lg-4 ml-auto text-center">
+            <?php echo do_shortcode("[mj20_contact_form]");  ?>
+          </div>
+          <div class="col-lg-4 ml-auto text-center">
+           
+          </div>
+
+        </div>
+        <br>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <i class="fas fa-phone fa-3x mb-3 sr-contact-1"></i>
+            <p>123-456-6789</p>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+            <i class="fas fa-envelope fa-3x mb-3 sr-contact-2"></i>
+            <p>
+              <a href="mailto:your-email@your-domain.com">feedback@mjcreative.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+<?php get_footer(); ?>
